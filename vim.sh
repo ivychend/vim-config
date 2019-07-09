4142c0236f861e311fdc8077bbb9b0d9017c8256
IvyChen <ivychend@gmail.com>
2019-07-09 13:50:32 +0800

    shell script to make .vim and vimrc take effect everywhere

---
+++ vim.sh
@@ -0,0 +1,8 @@
+# this file maybe renamed to vim.ew for vim_ew, ew means everywhere
+# you can source vim_ew where .vim cantain to make vimrc and vim add-on effect
+
+MY_VIM_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+export MYVIMRC=$MY_VIM_PATH/vimrc		
+export VIMINIT="let &rtp='$MY_VIM_PATH,' . &rtp
+so $MYVIMRC"					
+
