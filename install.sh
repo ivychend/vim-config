555ceeb315e6e48a1e57ef3e1dd5134a335e01b1
IvyChen <ivychend@gmail.com>
2019-08-01 15:52:10 +0800

    download and install univeral-ctags add plugged dir to .gitignore

--- install.sh
+++ install.sh
@@ -28,12 +28,27 @@ GLOBAL_VER=global-6.6.3
 GLOBAL=$GLOBAL_VER.tar.gz                   # install lastest version
 GLOBAL_URL=https://ftp.gnu.org/pub/gnu/global/$GLOBAL
 wget -c $GLOBAL_URL
-tar xf $GLOBAL_VER
+tar xf $GLOBAL
 cd $GLOBAL_VER
 ./configure --prefix=$EX_INSTALL            # added --prefix=dir config install dir, changed install dir  
 make && make install                        # need sudo or root by default, cause default install dir belong to root
 # use vim-plug install ludovicchabant/vim-gutentags skywind3000/gutentags_plus so that we do not need to copy *.vim anymore
 #cp gtags-cscope.vim,gtags.vim $VIM_DIR/plugin/
 
+# install universal ctags
+git clone https://github.com/universal-ctags/ctags.git
+cd ctags
+./autogen.sh                                # depend on autoconf, must install before
+./configure                                 # --prefix=/where/you/want # defaults to /usr/local
+make                                        # compile
+sudo make install                           # install universal ctags to /usr/local/,need sudo 
+
 # install pygments, for gtags
 pip install pygments
+
+# install nodejs and npm, needed by vim coc.vim
+sudo apt-get install curl python-software-properties
+# version 12.x
+# curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+sudo apt-get install -y nodejs
